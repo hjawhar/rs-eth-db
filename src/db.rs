3345a6bb5e9e6a517fe0ebe1da8a724ecb14d31d
Hassan Jawhar <hassanjawhar@protonmail.com>
2023-01-05 13:53:53 +0300

    Added a bunch of commands, TODO insert transactions

--- src/db.rs
+++ src/db.rs
@@ -1,13 +1,8 @@
 use crate::*;
-use diesel::result::Error;
-use diesel::sql_types::{BigInt, Int8};
 use diesel::{connection::Connection, PgConnection};
-use diesel::{prelude::*, *};
 use dotenv::dotenv;
-use ethers::types::{Block, H256, U64};
+use ethers::types::{Block, Transaction};
 use futures::lock::Mutex;
-use models::DBBlock;
-use schema::{self, blocks::dsl};
 use std::env;
 pub struct Database {
     pub connection: Arc<Mutex<PgConnection>>,
@@ -24,30 +19,70 @@ impl Database {
         }
     }
 
-    pub async fn simple_query(&self) {
+    pub async fn get_last_block_count(&self) -> i64 {
         let conn = &mut *(self.connection.lock().await);
-        let users = sql_query("SELECT COUNT(*) from blocks")
-            .load::<Count>(conn)
-            .unwrap();
-
-        println!("{:?}", users);
+        use crate::schema::blocks::dsl::*;
+        let count = blocks.count().get_result(conn).unwrap();
+        count
     }
 
-    pub fn insert_block(&self, block: &Block<H256>) {
-        println!("{:#?}", block.number.unwrap());
-        //
-        // Implement Logic
-        //
-        //-> Result<usize, Error>
-        // let conn = &mut *(self.connection);
-        // let result = insert_into(dsl::blocks)
-        //     .values((
-        //         dsl::number.eq(BigInt::from(0)), //block.number.unwrap_or(U64::from(0))
-        //         dsl::hash.eq(block.hash.unwrap_or("")),
-        //         dsl::timestamp.eq(block.timestamp),
-        //     ))
-        //     .execute(conn);
-        // //    .unwrap();
-        // result
+    pub async fn get_blocks_count_raw(&self) -> Vec<Count> {
+        let conn = &mut *(self.connection.lock().await);
+        let count = sql_query("SELECT COUNT(*) from blocks")
+            .load::<Count>(conn)
+            .unwrap();
+        count
+    }
+
+    pub async fn get_last_block(&self) -> Option<models::Block> {
+        let conn = &mut *(self.connection.lock().await);
+        use crate::schema::blocks::dsl::*;
+        let block = blocks
+            .order_by(number.desc())
+            .first(conn)
+            .optional()
+            .unwrap();
+        block
+    }
+
+    pub async fn get_blocks(&self) -> Vec<models::Block> {
+        let conn = &mut *(self.connection.lock().await);
+        let blocks = sql_query("SELECT * from blocks")
+            .load::<models::Block>(conn)
+            .unwrap();
+
+        blocks
+    }
+
+    pub async fn insert_block(&self, block: &Block<Transaction>) {
+        use crate::schema::blocks::dsl::*;
+        use diesel::insert_into;
+        println!(
+            "Block number: {} - Total transactions: {}",
+            block.number.unwrap(),
+            block.transactions.len()
+        );
+        let conn = &mut *(self.connection.lock().await);
+        let block_number = block.number.unwrap().to_string().parse::<i64>().unwrap();
+        let block_hash = block.hash.unwrap().to_string();
+        let block_timestamp = block.timestamp.to_string().parse::<i64>().unwrap();
+        let block_value = (
+            number.eq(block_number),
+            hash.eq(block_hash),
+            timestamp.eq(block_timestamp),
+        );
+        let result = insert_into(blocks).values(block_value).execute(conn);
+        match result {
+            Ok(_) => {
+                println!("Successfully added block {}", block.number.unwrap());
+            }
+            Err(err) => {
+                println!("Error adding block {} {}", err, block.number.unwrap());
+            }
+        }
+
+        for tx in block.transactions.iter() {
+            println!("{}", tx.hash);
+        }
     }
 }
