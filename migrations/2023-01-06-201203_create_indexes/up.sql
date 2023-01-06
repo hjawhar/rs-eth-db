ee7a18e033012cfacf7094ad2c4b6f13cdd2b8e8
hjawhar <hassanjawhar@protonmail.com>
2023-01-06 23:59:31 +0300

    Updated indexing script

--- migrations/2023-01-06-201203_create_indexes/up.sql
+++ migrations/2023-01-06-201203_create_indexes/up.sql
@@ -1,4 +1,4 @@
 -- Your SQL goes here
-create index sender_index on transactions(sender);
-create index receiver_index on transactions(receiver);
-create index hash_index on transactions(hash);
+create index IF NOT EXISTS sender_index on transactions(sender);
+create index IF NOT EXISTS receiver_index on transactions(receiver);
+create index IF NOT EXISTS hash_index on transactions(hash);
